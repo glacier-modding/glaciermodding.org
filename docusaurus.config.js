727d16549727e0f893b9af8f2cb998802f759d44
Notexe <Notexe@users.noreply.github.com>
2022-07-21 10:16:14 +0000

    Prettified Code!

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,18 +33,18 @@ module.exports = {
     },
     themes: [
         [
-          require.resolve("@easyops-cn/docusaurus-search-local"),
-          {
-            // ... Your options.
-            // `hashed` is recommended as long-term-cache of index file is possible.
-            hashed: true,
-            indexBlog: false,
-            docsRouteBasePath: "/",
-            // For Docs using Chinese, The `language` is recommended to set to:
-            // ```
-            // language: ["en", "zh"],
-            // ```
-          },
+            require.resolve("@easyops-cn/docusaurus-search-local"),
+            {
+                // ... Your options.
+                // `hashed` is recommended as long-term-cache of index file is possible.
+                hashed: true,
+                indexBlog: false,
+                docsRouteBasePath: "/",
+                // For Docs using Chinese, The `language` is recommended to set to:
+                // ```
+                // language: ["en", "zh"],
+                // ```
+            },
         ],
     ],
     presets: [
