9613b7280ca0baa94d2c81a00d8aec53ae31e315
Notexe <josh@notex.app>
2023-10-03 19:34:33 +1000

    Don't modify tool-versions.json on error

--- fetch-tool-versions.js
+++ fetch-tool-versions.js
@@ -7,10 +7,10 @@ const fetchVersion = async (repo) => {
             `https://api.github.com/repos/${repo}/releases/latest`,
         )
         const data = await response.json()
-        return data.tag_name || "v0.0.0"
+        return data.tag_name
     } catch (error) {
         console.error(`Error fetching the latest release for ${repo}:`, error)
-        return "v0.0.0"
+        throw error
     }
 }
 
@@ -18,9 +18,23 @@ const repos = ["glacier-modding/G2WwiseDataTool", "glacier-modding/RPKG-Tool"]
 
 ;(async () => {
     const versions = {}
+    let hasError = false
+
     for (const repo of repos) {
-        const name = repo.split("/").pop()
-        versions[name] = await fetchVersion(repo)
+        try {
+            const name = repo.split("/").pop()
+            versions[name] = await fetchVersion(repo)
+        } catch (error) {
+            hasError = true
+            break
+        }
     }
-    fs.writeFileSync("./tool-versions.json", JSON.stringify(versions))
+
+    if (!hasError) {
+        fs.writeFileSync("./tool-versions.json", JSON.stringify(versions))
+    } else {
+        console.log(
+            "One or more versions couldn't be fetched. File will not be changed.",
+        )
+    }
 })()
