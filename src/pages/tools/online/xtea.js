62c89319d6ecb2163a5a9c39370591a49da1a168
Notexe <josh@notex.app>
2024-01-27 23:35:17 +1000

    Goodbye xtea tool for now until I can bother fixing you

--- src/pages/tools/online/xtea.js
+++ src/pages/tools/online/xtea.js
@@ -1,12 +1,8 @@
-import React, { useEffect } from "react"
+import React from "react"
 import Layout from "@theme/Layout"
+import Admonition from '@theme/Admonition';
 
 export default function Xtea() {
-    useEffect(() => {
-        // Stupid workaround to make the xtea tool work, breaks the page entirely in development mode
-        // Also seems to break the theme colour toggle and search bar for some reason
-        window.location.reload()
-    }, [])
     return (
         <Layout
             title="Home"
@@ -14,7 +10,13 @@ export default function Xtea() {
         >
             <div>
                 <div className="container">
-                    <main></main>
+                    <main>
+                        <div className="container margin-vert--lg">
+                            <Admonition type="info">
+                                <p>Sorry, this tool is unavailable at the moment. If you wish to convert "packagedefinition.txt" or "thumbs.dat" into plaintext, please use the RPKG Tool instead.</p><p>For automatic updating of patch levels, it is best to use the <a href="https://www.nexusmods.com/hitman3/mods/200">Simple Mod Framework</a> for managing your mods.</p>
+                            </Admonition>
+                        </div>
+                    </main>
                 </div>
             </div>
         </Layout>
